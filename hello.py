3bc1143bc6ab2e15f339b3c98f774a45aad2dc60
Santiago Bello <santiago.bello@mercadolibre.com.co>
2025-02-06 16:32:14 -0500

    :sparkles: Add index and hello route

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+@app.route("/index")
+def index() -> str:
+    return "<h1>Página de inicio.</h1>"
+
 @app.route("/hello")
 def hello() -> str:
-    return "Hola mundo"
+    return "<h1>Hola mundo.</h1>"
